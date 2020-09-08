2818862f48f0718dca3cb0af6593e268b4a002ef
abhishek-pokhriyal <pokhi@coloredcow.com>
2020-09-08 18:04:16 +0530

    add missing files

---
+++ includes/class-wpb-autoloader.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * WP Plugin Boilerplate Autoloader.
+ *
+ * @package WP Plugin Boilerplate\Classes
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Autoloader class.
+ */
+class WPB_Autoloader {
+
+	/**
+	 * Path to the includes directory.
+	 *
+	 * @var string
+	 */
+	private $include_path = '';
+
+	/**
+	 * The Constructor.
+	 */
+	public function __construct() {
+		if ( function_exists( '__autoload' ) ) {
+			spl_autoload_register( '__autoload' );
+		}
+
+		spl_autoload_register( array( $this, 'autoload' ) );
+
+		$this->include_path = untrailingslashit( plugin_dir_path( WPB_PLUGIN_FILE ) ) . '/includes/';
+	}
+
+	/**
+	 * Take a class name and turn it into a file name.
+	 *
+	 * @param  string $class Class name.
+	 * @return string
+	 */
+	private function get_file_name_from_class( $class ) {
+		return 'class-' . str_replace( '_', '-', $class ) . '.php';
+	}
+
+	/**
+	 * Include a class file.
+	 *
+	 * @param  string $path File path.
+	 * @return bool Successful or not.
+	 */
+	private function load_file( $path ) {
+		if ( $path && is_readable( $path ) ) {
+			include_once $path;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Auto-load WPB classes on demand to reduce memory consumption.
+	 *
+	 * @param string $class Class name.
+	 */
+	public function autoload( $class ) {
+		$class = strtolower( $class );
+
+		if ( 0 !== strpos( $class, 'wpb_' ) ) {
+			return;
+		}
+
+		$file = $this->get_file_name_from_class( $class );
+		$path = '';
+
+		if ( 0 === strpos( $class, 'wpb_some_class_' ) ) {
+			$path = $this->include_path . 'some_class_dir/';
+		} elseif ( 0 === strpos( $class, 'wpb_another_class_' ) ) {
+			$path = $this->include_path . 'another_class_dir/';
+		}
+
+		if ( empty( $path ) || ! $this->load_file( $path . $file ) ) {
+			$this->load_file( $this->include_path . $file );
+		}
+	}
+}
+
+new WPB_Autoloader();
